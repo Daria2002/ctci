d0aafaafa1d330522dcafc9dd24a4add305c3487
Daria2002 <daria.matkovic@fer.hr>
2020-04-05 14:53:29 +0200

    Add merge sort implementation and few fixes,

--- CheckPermutation.cpp
+++ CheckPermutation.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <vector>
 
-bool are_permutations(const std::string&, const std::string&);
+bool are_permutations(std::string&, std::string&);
 
 /**
  * Given two strings, write a method to decide if one is a permutation of the other.
@@ -23,13 +23,42 @@ int main() {
     return 0;
 }
 
-void merge(std::string str, int left, int middle, int right) {
+void merge(std::string& str, int left_index, int middle_index, int right_index) {
+    const int size_left = middle_index - left_index + 1; // left part contains middle element
+    const int size_right = right_index - middle_index;
 
+    std::string left_part = str.substr(left_index, size_left);
+    std::string right_part = str.substr(middle_index + 1, size_right);
+
+    int iter_left(0), iter_right(0), iter_orig(left_index);
+    while (iter_left < size_left && iter_right < size_right)
+    {
+        if(left_part.at(iter_left) < right_part.at(iter_right)) {
+            str[iter_orig] = left_part.at(iter_left);
+            iter_left++;
+        } else {
+            str[iter_orig] = right_part.at(iter_right);
+            iter_right++;
+        }
+        iter_orig++;
+    }
+    
+    while(iter_left < size_left) {
+        str[iter_orig] = left_part.at(iter_left);
+        iter_left++;
+        iter_orig++;
+    }
+
+    while(iter_right < size_right) {
+        str[iter_orig] = right_part.at(iter_right);
+        iter_right++;
+        iter_orig++;
+    }
 }
 
-void merge_sort(std::string& str, const int& left, const int& right) {
+void merge_sort(std::string& str, int left, int right) {
     if(left < right) {
-        const int middle = (left + right) / 2;
+        const int middle = left + (right - left) / 2;
         merge_sort(str, left, middle);
         merge_sort(str, middle + 1, right);
         merge(str, left, middle, right);
@@ -38,7 +67,7 @@ void merge_sort(std::string& str, const int& left, const int& right) {
 
 // merge sort
 void sort_string(std::string& str) {
-    merge_sort(str, 0, str.size());
+    merge_sort(str, 0, str.size() - 1);
 }
 
 bool are_permutations(std::string& first, std::string& second) {
