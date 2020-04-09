8dfe9f70db76152641bbde58fbe3f74e31ae73aa
Daria2002 <daria.matkovic@fer.hr>
2020-04-09 09:14:04 +0200

    Add MatrixHelper file used for reading and writing matrix to file. Start working on ZeroMatrix problem.

--- RotateMatrix.cpp
+++ RotateMatrix.cpp
@@ -3,10 +3,9 @@
 #include <string>
 #include <fstream>
 #include <sstream>
+#include "MatrixHelper.h"
 
-bool initalize_marix(const std::string&, std::vector<std::vector<int>>&);
 void rotate_matrix(std::vector<std::vector<int>>&);
-bool write_matrix_to_file(const std::string&, const std::vector<std::vector<int>>&);
 bool check_dimensions(const std::vector<std::vector<int>>&);
 
 /**
@@ -19,13 +18,14 @@ bool check_dimensions(const std::vector<std::vector<int>>&);
  * Input matrix should separate elements with ,
  */
 int main(int argc, char** argv) {
+    MatrixFile::MatrixManager mm;
     if(argc != 2) {
         std::cout << "Please provide one parameter in program. Parameter should be file path." << std::endl;
         return 0;
     }
     std::vector<std::vector<int>> matrix;
     const std::string file_name = argv[1];
-    if(initalize_marix(file_name, matrix) == false) {
+    if(mm.initalize_matrix(file_name, matrix) == false) {
         std::cout << "Matrix is not initailized." << std::endl;
         return 0;
     }
@@ -37,7 +37,7 @@ int main(int argc, char** argv) {
 
     rotate_matrix(matrix);
 
-    if(write_matrix_to_file(file_name, matrix) == false) {
+    if(mm.write_matrix_to_file(file_name, matrix) == false) {
          std::cout << "Error while writing matrix to file." << std::endl;
     }
     std::cout << "Rotated matrix is written to " << file_name << std::endl;
@@ -49,41 +49,6 @@ bool check_dimensions(const std::vector<std::vector<int>>& matrix) {
         if(column.size() != row_size) {
             return false;
         }
-    }
-    return true;
-}
-
-bool split(const std::string& line, const char delimiter, std::vector<int>& elements) {
-    std::vector<std::string> tokens;
-    std::string token;
-    std::istringstream ss(line);
-    std::string tmp;
-    int number;
-    while(std::getline(ss, tmp, delimiter)) {
-        try {
-            number = std::stoi(tmp);
-        } catch(const std::exception& e) {
-            return false;
-        }
-        elements.push_back(number);
-    }
-    return true;
-}
-
-bool initalize_marix(const std::string& file_name, std::vector<std::vector<int>>& matrix) {
-    std::ifstream file;
-    file.open(file_name);
-    if(file.is_open() == false) {
-        return false;
-    }
-    std::vector<int> row_elements;
-    std::string line;
-    while(std::getline(file, line)) {
-        if(split(line, ',', row_elements) == false) {
-            return false;
-        }
-        matrix.push_back(row_elements);
-        row_elements.clear();
     }
     return true;
 }
@@ -106,25 +71,4 @@ void rotate_matrix(std::vector<std::vector<int>>& matrix) {
             matrix[last][i] = top; // top -> left
         }
     }
-}
-
-bool write_matrix_to_file(const std::string& file_name, const std::vector<std::vector<int>>& matrix) {
-    std::ofstream file(file_name);
-    if(!file.is_open()) {
-        return false;
-    }
-    const int n = matrix.size();
-    std::string row;
-    for(int i = 0; i < n; i++) {
-        for(int j = 0; j < n; j++) {
-            row.append(std::to_string(matrix[i][j]));
-            if(j != n - 1) {
-                row.append(",");
-            }
-        }
-        file << row << std::endl;
-        row.clear();
-    }
-    file.close();
-    return true;
 }
---
+++ ZeroMatrix.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * Write an algorithm such that if an element in MxN matrix is 0,
+ * its entire row and column are set to 0.
+ */
+int main() {
+
+    
+
+}
