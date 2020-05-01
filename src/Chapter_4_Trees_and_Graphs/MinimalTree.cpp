e9004d9f900e7bb46b7ddc3afc272368dfbdaea9
Daria2002 <daria.matkovic@fer.hr>
2020-05-01 15:15:41 +0200

    Minimal BST functionality is itroduced. TODO: print BST depth and print BST content.

--- src/Chapter_4_Trees_and_Graphs/MinimalTree.cpp
+++ src/Chapter_4_Trees_and_Graphs/MinimalTree.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <vector>
-#include <bits/stdc++.h> 
+#include <algorithm>
 
 std::vector<int> get_values() {
     std::vector<int> v;
@@ -23,29 +23,24 @@ class TreeNode {
         int _value;
         TreeNode* left;
         TreeNode* right;
+        TreeNode() {}
         TreeNode(int value) : _value(value) {}
 };
 
-TreeNode create_minimal_BST(const std::vector<int>& values, const int start, const int end) {
-    if(values[start] == values[end]) {
-        TreeNode node(values[start]);
-        return node;
+TreeNode* create_minimal_BST(const std::vector<int>& values, const int start, const int end) {
+    if(start > end) {
+        return nullptr;
     }
-
     int middle_index = start + (end - start) / 2;
-
-    TreeNode root(values[middle_index]);
-    TreeNode left = create_minimal_BST(values, start, middle_index);
-    TreeNode right = create_minimal_BST(values, middle_index, end);
-
-    root.left = new TreeNode(left._value);
-    root.right = new TreeNode(right._value);
-    
+    TreeNode* root = new TreeNode(values[middle_index]);
+    root -> left = create_minimal_BST(values, start, middle_index - 1);
+    root -> right = create_minimal_BST(values, middle_index + 1, end);
     return root;
 }
 
-TreeNode create_minimal_BST(const std::vector<int>& values) {
-    return create_minimal_BST(values, 0, values.size() - 1);
+TreeNode* create_minimal_BST(const std::vector<int>& values) {
+    TreeNode* node = create_minimal_BST(values, 0, values.size() - 1);
+    return node;
 }
 
 /**
@@ -57,7 +52,8 @@ int main() {
                 "Program for creating a binary search tree with minimal height\n"
                 "=============================================================\n";
     std::vector<int> values = get_values();
+    // std::vector<int> values = {1, 3, 4, 5, 6}; // example
     std::sort(values.begin(), values.end(), std::less<int>());
-    create_minimal_BST(values);
+    TreeNode* root = create_minimal_BST(values);
     std::cout << "Minimal BST is created.\n";
 }
