03c5c87db3a1e9b10faa2cfff157a2091f0a8842
Daria2002 <daria.matkovic@fer.hr>
2020-05-06 12:10:23 +0200

    Add argument in launch.json, start implementation for BuildOrder problem and add input file for BuildOrder problem.

---
+++ src/Chapter_4_Trees_and_Graphs/BuildOrder.cpp
@@ -0,0 +1,107 @@
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <filesystem>
+#include <map>
+#include <algorithm>
+
+class Project {
+    public:
+        Project() {}
+        Project(std::string _name) : name(_name) {}
+        std::string name;
+        void add_child(const Project& project) {
+            children.push_back(project);
+        }
+        std::vector<Project> children;
+        int dependencies_to_be_build;
+};
+
+class Graph {
+    public:
+        std::vector<Project> nodes;
+        std::map<std::string, Project> map_nodes;
+        void add_node(const Project project) {
+            nodes.push_back(project);
+            map_nodes[project.name] = project;
+        }
+
+        Project get_node(const std::string& node_name) {
+            return map_nodes[node_name];
+        }
+};
+
+std::vector<std::string> separate_string(const std::string& str, const std::string& regex) {
+    std::vector<std::string> values;
+
+    return values;
+}
+
+std::vector<std::pair<std::string, std::string>> make_dependency_pairs(std::vector<std::string> dependencies) {
+    std::vector<std::pair<std::string, std::string>> pairs;
+    return pairs;
+}
+
+bool get_child(std::vector<std::pair<std::string, std::string>> dependency_pairs, Project child) {
+    return true;
+}
+
+Graph build_graph(std::string project_names, std::string project_dependencies) {
+    Graph graph;
+    std::vector<std::string> names = separate_string(project_names, ", ");
+    std::vector<std::string> dependencies = separate_string(project_dependencies.substr(1, project_dependencies.size() - 2), "), (");
+    std::vector<std::pair<std::string, std::string>> dependency_pairs = make_dependency_pairs(dependencies);
+    
+    std::for_each(names.begin(), names.end(), [&](std::string name) {
+        Project project(name);
+        if(Project child; get_child(dependency_pairs, child)) {
+            project.add_child(child);
+        }
+        graph.add_node(project);
+    });
+    return graph;
+}
+
+Graph get_graph(const std::string& input_file) {
+    std::ifstream file(input_file);
+    std::string project_names;
+    std::string project_dependencies;
+    std::getline(file, project_names);
+    std::getline(file, project_dependencies);
+    return build_graph(project_names, project_dependencies);
+}
+
+std::vector<Project> get_build_order(const Graph& graph) {
+    std::vector<Project> build_order;
+
+    return build_order;
+}
+
+void print_build_order(const std::vector<Project>& build_order) {
+    std::cout << "build order:\n";
+    for(int i = 0; i < build_order.size(); i++) {
+        std::cout << build_order[i].name;
+        if(i != build_order.size() - 1) {
+            std::cout << ", ";
+        } else {
+            std::cout << '\n';
+        }
+    }
+}
+
+/**
+ * You are given a list of projects and a list of dependencies (which is a list of pairs of projects, 
+ * where the second project is dependent on the first project). All of a project's dependencies must be
+ * built before the project is. Find a build order that will allow the projects to be built. If there is
+ * no valid build order, return error.
+ * EXAMPLE:
+ * Input:
+ *  projects: a, b, c, d, e, f
+ *  dependencies: (a, d), (f, b), (b, d), (f, a), (d, c)
+ * Output: f, e, a, b, d, c
+ */
+int main(int argc, char** argv) {
+    Graph graph = get_graph(argv[1]);
+    std::vector<Project> build_order = get_build_order(graph);
+    print_build_order(build_order);
+}
