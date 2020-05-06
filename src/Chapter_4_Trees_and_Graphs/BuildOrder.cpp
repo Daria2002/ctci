2c645e24b4e75d832a4da6e95ed067a47b5416a5
Daria2002 <daria.matkovic@fer.hr>
2020-05-06 15:46:52 +0200

    Add implementation for reading projects and dependencies. TODO: fix adding children to projects.

--- src/Chapter_4_Trees_and_Graphs/BuildOrder.cpp
+++ src/Chapter_4_Trees_and_Graphs/BuildOrder.cpp
@@ -4,6 +4,7 @@
 #include <filesystem>
 #include <map>
 #include <algorithm>
+#include <sstream>
 
 class Project {
     public:
@@ -12,9 +13,10 @@ class Project {
         std::string name;
         void add_child(const Project& project) {
             children.push_back(project);
+            dependencies_to_be_build++;
         }
         std::vector<Project> children;
-        int dependencies_to_be_build;
+        int dependencies_to_be_build = 0;
 };
 
 class Graph {
@@ -25,25 +27,42 @@ class Graph {
             nodes.push_back(project);
             map_nodes[project.name] = project;
         }
-
         Project get_node(const std::string& node_name) {
             return map_nodes[node_name];
         }
 };
 
-std::vector<std::string> separate_string(const std::string& str, const std::string& regex) {
+std::vector<std::string> separate_string(std::string str, const std::string& regex) {
     std::vector<std::string> values;
-
+    std::size_t pos = 0;
+    std::string token;
+    while ((pos = str.find(regex)) != std::string::npos) {
+        token = str.substr(0, pos);
+        values.push_back(token);
+        str = str.substr(pos + regex.length());
+    }
+    values.push_back(str); // last element
     return values;
 }
 
 std::vector<std::pair<std::string, std::string>> make_dependency_pairs(std::vector<std::string> dependencies) {
     std::vector<std::pair<std::string, std::string>> pairs;
+    std::for_each(dependencies.begin(), dependencies.end(), [&](auto el) {
+        std::vector<std::string> values = separate_string(el, ", ");
+        pairs.push_back(std::make_pair(values[0], values[1]));
+    });
     return pairs;
 }
 
-bool get_child(std::vector<std::pair<std::string, std::string>> dependency_pairs, Project child) {
-    return true;
+bool get_child(std::vector<std::pair<std::string, std::string>> dependency_pairs, const std::string& parent, std::string& child) {
+    auto it = std::find_if(dependency_pairs.begin(), dependency_pairs.end(), 
+    [&](const std::pair<std::string, std::string>& pair) {
+        if(pair.first == parent) {
+            child = pair.second;
+            return true;
+        }
+    });
+    return it != dependency_pairs.end();
 }
 
 Graph build_graph(std::string project_names, std::string project_dependencies) {
@@ -54,11 +73,15 @@ Graph build_graph(std::string project_names, std::string project_dependencies) {
     
     std::for_each(names.begin(), names.end(), [&](std::string name) {
         Project project(name);
-        if(Project child; get_child(dependency_pairs, child)) {
-            project.add_child(child);
-        }
         graph.add_node(project);
     });
+
+    std::for_each(graph.nodes.begin(), graph.nodes.end(), [&](auto& project) {
+        if(std::string child; get_child(dependency_pairs, project.name, child)) {
+            project.add_child(graph.map_nodes[child]);
+        }
+    });
+
     return graph;
 }
 
