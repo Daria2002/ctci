9c1c2e2539542d3739c01b798884fedaa10451cc
Daria Matković <dariamatkovicdaria@gmail.com>
2020-05-11 14:42:36 +0200

    Add solution using nodes with link to the parent.

--- src/Chapter_4_Trees_and_Graphs/FirstCommonAncestor.cpp
+++ src/Chapter_4_Trees_and_Graphs/FirstCommonAncestor.cpp
@@ -5,6 +5,7 @@ class Node {
     public:
         Node() {}
         Node(int _value) : value(_value) {}
+        Node(int _value, std::weak_ptr<Node> _parent) : value(_value), parent(_parent) {}
         std::shared_ptr<Node> right;
         std::shared_ptr<Node> left;
         std::weak_ptr<Node> parent;
@@ -13,19 +14,73 @@ class Node {
 
 using NodePtr = std::shared_ptr<Node>;
 
-// TODO: understand why you need to send by ref to change root and root->left
-// it's a pointer and why sending it by value doesn't change root and root->left?
 void create_tree(NodePtr& root) {
     root = std::make_shared<Node>(20);
-    root -> left = std::make_shared<Node>(25);
+    root -> left = std::make_shared<Node>(25, root);
+    root -> right = std::make_shared<Node>(14, root);
+    NodePtr tmp;
+    tmp = root -> left;
+    tmp -> left = std::make_shared<Node>(17, tmp);
+    tmp -> left -> left = std::make_shared<Node>(16, tmp -> left);
+    tmp -> left -> left -> right = std::make_shared<Node>(15, tmp -> left -> left);
+    tmp -> right = std::make_shared<Node>(5, tmp);
+    tmp -> right -> left = std::make_shared<Node>(18, tmp -> right);
+    tmp -> right -> left -> right = std::make_shared<Node>(1, tmp -> right -> left);
+    tmp -> right -> right = std::make_shared<Node>(3, tmp -> right);
+    tmp -> right -> right -> right = std::make_shared<Node>(21, tmp -> right -> right);
+    tmp = tmp -> right -> right -> right;
+    tmp -> left = std::make_shared<Node>(4, tmp);
+    tmp -> left -> left = std::make_shared<Node>(19, tmp -> left);
+    tmp -> left -> right = std::make_shared<Node>(12, tmp -> left);
+    tmp = root -> right;
+    tmp -> left = std::make_shared<Node>(13, tmp);
+    tmp -> left -> right = std::make_shared<Node>(7, tmp -> left);
+    tmp -> right = std::make_shared<Node>(27, tmp);
 }
 
-std::shared_ptr<Node> fca_links_to_parents(NodePtr& root, NodePtr& successor1, NodePtr& successor2) {
-
+int depth(NodePtr node) {
+    int depth = 0;
+    while(node != nullptr) {
+        node = (node -> parent).lock();
+        depth++;
+    }
+    return depth;
 }
 
-void get_node(const int value, NodePtr& root, NodePtr& node) {
+void go_up_by_delta(NodePtr& node, int delta) {
+    int old_val = node -> value;
+    while (delta > 0 && node != nullptr) {
+        node = (node -> parent).lock();
+        delta--;
+    }  
+}
 
+NodePtr fca_links_to_parents(NodePtr& successor1, NodePtr& successor2) {
+    int depth_delta = depth(successor1) - depth(successor2);
+    // first is deeper node, second is shellower node
+    NodePtr first = (depth_delta > 0) ? successor1 : successor2;
+    NodePtr second = (depth_delta > 0) ? successor2 : successor1;
+    go_up_by_delta(first, std::abs(depth_delta));
+    while(first != nullptr && second != nullptr && first != second) {
+        first = (first -> parent).lock();
+        second = (second -> parent).lock();
+    }
+    return ((first == nullptr || second == nullptr) ? nullptr : first);
+}
+
+bool get_node(const int value, NodePtr& root, NodePtr& node) {
+    if(root == nullptr) {
+        return false;
+    }
+    if(root -> value == value) {
+        node = root;
+        return true;
+    }
+    if(get_node(value, root -> left, node))
+        return true;
+    if(get_node(value, root -> right, node)) 
+        return true;
+    return false;
 }
 
 /**
@@ -40,31 +95,21 @@ int main(int argc, char** argv) {
     std::cout << "=====================================\n"
                  "Program to find first common ancestor\n"
                  "=====================================\n";
-    if(argc < 2) {
-        std::cout << "Please provide one argument that represents path to input file\n";
-        return 0;
-    }
     std::cout << "Enter method number for finding first common ancestor:\n";
     std::cout << "1 - With links to Parents\n";
     std::cout << "2 - With links to Parents - approved\n";
     std::cout << "3 - Without links to Parents\n";
     std::cout << "4 - Optimized\n";
-    NodePtr root;
-    NodePtr successor_node1;
-    NodePtr successor_node2;
-    NodePtr fca;
-    int method, successor_name1, successor_name2;
+    NodePtr root, fca;
+    int method = 1;
     create_tree(root);
-    std::cin >> method;
-    std::cout << "Enter first successor value\n";
-    std::cin >> successor_name1;
-    std::cout << "Enter second successor value\n";
-    std::cin >> successor_name2;
-    get_node(successor_name1, root, successor_node1);
-    get_node(successor_name2, root, successor_node2);
+    NodePtr first_successor, second_successor;
+    get_node(4, root, first_successor);
+    get_node(15, root, second_successor);
+    // std::cin >> method;
     switch (method) {
     case 1:
-        fca = fca_links_to_parents(root, successor_node1, successor_node2);
+        fca = fca_links_to_parents(first_successor, second_successor);
         break;
     case 2:
         // fca = fca_links_to_parents_approved();
@@ -79,4 +124,5 @@ int main(int argc, char** argv) {
         std::cout << "None of the provided methods have not been selected\n";
         break;
     }
+    std::cout << "First common ancestor has value " << (fca -> value) << '\n';
 }
