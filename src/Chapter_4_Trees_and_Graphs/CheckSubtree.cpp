8a9b4ecad0705b47ecd694cf2168989f91fde3f6
Daria Matković <dariamatkovicdaria@gmail.com>
2020-05-14 16:11:51 +0200

    Add solution using recursive function.

--- src/Chapter_4_Trees_and_Graphs/CheckSubtree.cpp
+++ src/Chapter_4_Trees_and_Graphs/CheckSubtree.cpp
@@ -5,7 +5,7 @@ class Node {
     public:
         Node() {}
         Node(int val) : value(val) {}
-        Node(int val, std::shared_ptr<Node> l, std::shared_ptr<Node> r) : value(val), left(l), right(r) {}
+        Node(int val, int l, int r) : value(val), left(std::make_shared<Node>(l)), right(std::make_shared<Node>(r)) {}
         std::shared_ptr<Node> left;
         std::shared_ptr<Node> right;
         int value;
@@ -14,24 +14,40 @@ class Node {
 using NodePtr = std::shared_ptr<Node>;
 
 void create_bigger_tree(NodePtr& root) {
-
+    root = std::make_shared<Node>(20, 25, 14);
+    root -> left -> left = std::make_shared<Node>(17, 30, 70);
+    root -> left -> right = std::make_shared<Node>(12, 40, 90);
+    root -> right -> left = std::make_shared<Node>(11, 50, 80);
+    root -> right -> right = std::make_shared<Node>(2, 60, 55);
 }
 
 void create_smaller_nonsubtree(NodePtr& root) {
-    
+    root = std::make_shared<Node>(11, 43, 3);
+    root -> left -> left = std::make_shared<Node>(1);
+    root -> left -> right = std::make_shared<Node>(2);
+    root -> right -> left = std::make_shared<Node>(4);
+    root -> right -> right = std::make_shared<Node>(5);
 }
 
 void create_smaller_subtree(NodePtr& root) {
-    
+    root = std::make_shared<Node>(17, 30, 70);
+    root = std::make_shared<Node>(12, 40, 90);
 }
 
-bool is_subtree(NodePtr root1, NodePtr root2) {
-    if(root1 == nullptr || root2 == nullptr) {
+bool is_subtree(const NodePtr& root1, const NodePtr& root2) {
+    // return true if both trees empty or if subtree empty because empty tree is subtree of any tree
+    if(root1 == nullptr && root2 == nullptr || root1 != nullptr && root2 == nullptr) {
+        return true;
+    }
+    // if subtree is not empty and tree is empty return false
+    if(root1 == nullptr) {
         return false;
     }
-    if(root1 != root2) {
+    // check left and right of the tree if current node in the tree is not same as the subtree root
+    if(root1 -> value != root2 -> value) {
         return is_subtree(root1 -> left, root2) || is_subtree(root1 -> right, root2);
     }
+    // root1 = root2
     return is_subtree(root1 -> left, root2 -> left) && is_subtree(root1 -> right, root2 -> right);
 }
 
@@ -45,7 +61,7 @@ int main() {
     NodePtr t1;
     NodePtr t2;
     create_bigger_tree(t1);
-    create_smaller_nonsubtree(t2);
-    // create_smaller_subtree(t2);
+    // create_smaller_nonsubtree(t2);
+    create_smaller_subtree(t2);
     std::cout << "T2 is " << (is_subtree(t1, t2) ? "" : "not ") << "a subtree of T1.\n";
 }
