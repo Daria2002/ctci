24d1e704ed8269dcdec2eac5e1b110a0bc7d5ef4
Daria2002 <daria.matkovic@fer.hr>
2020-05-16 21:45:29 +0200

    Add some basics to RandomNode.cpp

--- src/Chapter_4_Trees_and_Graphs/RandomNode.cpp
+++ src/Chapter_4_Trees_and_Graphs/RandomNode.cpp
@@ -6,29 +6,57 @@ class TreeNode {
         TreeNode() {};
         TreeNode(int val) : value(val) {}
         int value;
+        int size;
         std::shared_ptr<TreeNode> left;
         std::shared_ptr<TreeNode> right;
+        // ask where to put duplicates (on left or right)
+        void insert(int val) {
+            if(val <= value) {
+                if(left == nullptr) {
+                    left = std::make_shared<TreeNode>(val);
+                } else {
+                    left -> insert(val);
+                }
+            } else {
+                if(right == nullptr) {
+                    right = std::make_shared<TreeNode>(val);
+                } else {
+                    right -> insert(val);
+                }
+            }
+            size++;
+        }
+        std::shared_ptr<TreeNode> find(int val) {
+            if(val == value) {
+                return std::shared_ptr<TreeNode>(this);
+            }
+            if(val < value) {
+                return left == nullptr ? nullptr : left -> find(val);
+            } else {
+                return right == nullptr ? nullptr : right -> find(val);
+            }
+        }
+        void delete_node(int val) {
+            if(value == val) {
+                delete this;
+            }
+            if(left -> value == val) {
+                left = nullptr;
+            } else if(right -> value == val) {
+                right = nullptr;
+            }
+            if(left -> value < val) {
+                left -> delete_node(val);
+            } else {
+                right -> delete_node(val);
+            }
+        }
+        std::shared_ptr<TreeNode> get_random_node() {
+            
+        }
 };
 
 using NodePtr = std::shared_ptr<TreeNode>;
-
-class BinarySearchTree {
-    public:
-        BinarySearchTree(NodePtr& r) : root(r) {}
-        NodePtr root;
-        void insert(int value) {
-
-        }
-        NodePtr find(int value) {
-
-        }
-        void delete_node(int value) {
-
-        }
-        NodePtr get_random_node() {
-
-        }
-};
 
 /**
  * You are implementing a binary search tree class from scratch, which, in addition to 
