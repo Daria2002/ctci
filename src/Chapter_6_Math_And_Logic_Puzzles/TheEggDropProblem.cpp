4390766a72f657b4d15a3c245efa1fc6ac935d5a
Daria2002 <daria.matkovic@fer.hr>
2020-05-26 06:38:33 +0200

    Start working on The Egg Drop Problem

---
+++ src/Chapter_6_Math_And_Logic_Puzzles/TheEggDropProblem.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+/**
+ * There is a building of 100 floors. If an egg drops from the Nth floor or above it will break.
+ * If it's dropped from any floor below, it will not break. You're given 2 eggs. Find N, and 
+ * minimize the number of drops for the worse case.
+ */
+int main() {
+
+}
