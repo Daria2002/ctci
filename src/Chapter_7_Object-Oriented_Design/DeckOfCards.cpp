8b3d60949c3d260796838b281a275a5d2bdd8467
Daria2002 <daria.matkovic@fer.hr>
2020-05-29 07:30:06 +0200

    Add solution for Deck Of Cards

--- src/Chapter_7_Object-Oriented_Design/DeckOfCards.cpp
+++ src/Chapter_7_Object-Oriented_Design/DeckOfCards.cpp
@@ -14,9 +14,16 @@ enum class Suit {
 template <typename T>
 class Deck {
     public:
+        bool two_cards_available() {
+            return dealt_index + 1 < cards.size();
+        }
+        bool one_card_available() {
+            return dealt_index < cards.size();
+        }
         T deal_card() {
+            T t = cards[dealt_index];
             dealt_index++;
-            return cards[dealt_index];
+            return t;
         }
         void set_deck_of_cards(std::vector<T> deck_of_cards) {
             cards = deck_of_cards;
@@ -47,12 +54,10 @@ class Deck {
 };
 
 class Card {
-    protected:
+    public:
         bool available = true;
         int face_value; // 11-Jack, 12-Queen, 13-King, 1-Ace, 2-10
         Suit suit;
-    public:
-        Card() = default;
         Card(int fv, Suit s) : face_value(fv), suit(s) {}
         virtual int value() = 0;
         Suit get_suit() {
@@ -66,6 +71,42 @@ class Card {
         }
         void mark_unavailable() {
             available = false;
+        }
+        void print() {
+            std::vector<std::string> face_values;
+            face_values.push_back("A");
+            face_values.push_back("1");
+            face_values.push_back("2");
+            face_values.push_back("3");
+            face_values.push_back("4");
+            face_values.push_back("5");
+            face_values.push_back("6");
+            face_values.push_back("7");
+            face_values.push_back("8");
+            face_values.push_back("9");
+            face_values.push_back("10");
+            face_values.push_back("J");
+            face_values.push_back("Q");
+            face_values.push_back("K");
+            std::cout << face_values[face_value - 1];
+            switch (suit)
+            {
+            case Suit::Club:
+                std::cout << "c";
+                break;
+            case Suit::Heart:
+                std::cout << "h";
+                break;
+            case Suit::Diamond:
+                std::cout << "d";
+                break;
+            case Suit::Spade:
+                std::cout << "s";
+                break;
+            default:
+                break;
+            }
+            std::cout << ' ';
         }
 };
 
@@ -84,20 +125,21 @@ class Hand {
         void add_card(T card) {
             cards.push_back(card);
         }
+        void print() {
+            for(T c : cards) {
+                c.print();
+            }
+        }
 };
 
 class BlackJackCard : public Card {
     public:
-        int face_value;
-        Suit suit;
-        BlackJackCard(int val, Suit s) : face_value(val), suit(s) {}
-        BlackJackCard() = default;
+        BlackJackCard(int val, Suit s) : Card::Card(val, s) {}
         int value() {
             if(is_ace()) return 1;
             else if(is_free_card()) return 10;
             return face_value;
         }
-    private:
         int min_val() {
             return value();
         }
@@ -126,7 +168,7 @@ class BlackJackHand : public Hand<BlackJackCard> {
                     max = score;
                 }
             }
-            return max == std::numeric_limits<int>::min() ? min : max;
+            return (max == std::numeric_limits<int>::min() ? min : max);
         }
         bool busted() {
             return score() > 21;
@@ -135,12 +177,129 @@ class BlackJackHand : public Hand<BlackJackCard> {
             return score() == 21;
         }
         bool is_black_jack() {
-            
+            if(cards.size() != 2) return false;
+            BlackJackCard first = cards[0];
+            BlackJackCard second = cards[1];
+            return (first.is_ace() && second.is_free_card() || (second.is_ace() && first.is_free_card()));
         }
     private:
+        void add_card_to_score_list(BlackJackCard card, std::vector<int>& scores) {
+            if(scores.size() == 0) {
+                scores.push_back(0);
+            }
+            int len = scores.size();
+            for(int i = 0; i < len; i++) {
+                int score = scores[i];
+                scores[i] = score + card.min_val();
+                if(card.min_val() != card.max_val()) {
+                    scores.push_back(score + card.max_val());
+                }
+            }
+        }
         std::vector<int> possible_scores() {
             // returns possible scores that this hand can achive
+            std::vector<int> scores;
+            if(cards.size() == 0) {
+                scores.push_back(0);
+                return scores;
+            }
+            for(BlackJackCard card : cards) {
+                add_card_to_score_list(card, scores);
+            }
+            return scores;
         }
+};
+
+class BlackJackAutomator {
+    public:
+        Deck<BlackJackCard> deck;
+        std::vector<BlackJackHand> hands;
+        const int HIT_UNTIL = 16; 
+        BlackJackAutomator(int n) : num_of_hands(n) {
+            for(int i = 0; i < num_of_hands; i++) {
+                hands.push_back(BlackJackHand());
+            }
+        }
+        bool deal_initial() {
+            for(BlackJackHand hand : hands) {
+                if(!deck.two_cards_available()) {
+                    return false;
+                }
+                BlackJackCard card1 = deck.deal_card();
+                BlackJackCard card2 = deck.deal_card();
+                hand.add_card(card1);
+                hand.add_card(card2);
+            }
+            return true;
+        }
+        std::vector<int> get_black_jacks() {
+            std::vector<int> winners;
+            for(int i = 0; i < hands.size(); i++) {
+                if(hands[i].is_black_jack()) {
+                    winners.push_back(i);
+                }
+            }
+            return winners;
+        }
+        bool play_hand(int i) {
+            BlackJackHand hand = hands[i];
+            return play_hand(hand);
+        }
+        bool play_hand(BlackJackHand& hand) {
+            while (hand.score() < HIT_UNTIL)
+            {
+                if(!deck.one_card_available()) {
+                    return false;
+                }
+                BlackJackCard card = deck.deal_card();
+                hand.add_card(card);
+            }
+            return true;
+        }
+        bool play_all_hands() {
+            for(BlackJackHand& hand : hands) {
+                if(!play_hand(hand)) return false;
+            }
+            return true;
+        }
+        std::vector<int> get_winners() {
+            std::vector<int> winners;
+            int winning_score = 0;
+            for(int i = 0; i < hands.size(); i++) {
+                BlackJackHand hand = hands[i];
+                if(!hand.busted()) {
+                    if(hand.score() > winning_score) {
+                        winning_score = hand.score();
+                        winners.clear();
+                        winners.push_back(i);
+                    } else if(hand.score() == winning_score) {
+                        winners.push_back(i);
+                    }
+                }
+            }
+            return winners;
+        }
+        void ini_deck() {
+            std::vector<BlackJackCard> cards;
+            for(int i = 1; i <= 13; i++) {
+                for(int j = 0; j <= 3; j++) {
+                    Suit suit = Suit(j);
+                    BlackJackCard card(i, suit);
+                    cards.push_back(card);
+                }
+            }
+            deck.set_deck_of_cards(cards);
+            deck.shuffle();
+        }
+        void print_hands_and_score() {
+            for(int i = 0; i < hands.size(); i++) {
+                std::cout << "Hand " << i << " (" << hands[i].score() << "): ";
+                hands[i].print();
+                std::cout << '\n';
+            }
+        }
+    private:
+        int num_of_hands;
 };
 
 /**
@@ -151,4 +310,40 @@ int main() {
     std::cout << "=================================================================================\n"
                  "This program is used for designing data structures for implementing deck of cards\n"
                  "=================================================================================\n";
+    int num_of_hands = 5;
+    BlackJackAutomator automator(num_of_hands);
+    automator.ini_deck();
+    bool success = automator.deal_initial();
+    if(!success) {
+        std::cout << "Error. Out of cards\n";
+    } else {
+        std::cout << "-- Initial --\n";
+        automator.print_hands_and_score();
+        std::vector<int> black_jacks = automator.get_black_jacks();
+        if(black_jacks.size() > 0) {
+            std::cout << "Blackjack at ";
+            for(int i : black_jacks) {
+                std::cout << i << ", ";
+            }
+            std::cout << '\n';
+        } else {
+            success = automator.play_all_hands();
+            if(!success) {
+                std::cout << "Error. Out of cards\n";
+            } else {
+                std::cout << "\n-- Completed Game --\n";
+                automator.print_hands_and_score();
+                std::vector<int> winners = automator.get_winners();
+                if(winners.size() > 0) {
+                    std::cout << "Winners: ";
+                    for(int i : winners) {
+                        std::cout << i << ", ";
+                    }
+                    std::cout << '\n';
+                } else {
+                    std::cout << "Draw. All players have busted\n";
+                }
+            }
+        }
+    }
 }
