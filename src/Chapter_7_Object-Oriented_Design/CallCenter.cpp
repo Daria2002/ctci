c77c54e467df7dd418a895e9434d75e118065bf9
Daria Matković <dariamatkovicdaria@gmail.com>
2020-05-29 12:14:10 +0200

    Add some basic implementation for Call Center

--- src/Chapter_7_Object-Oriented_Design/CallCenter.cpp
+++ src/Chapter_7_Object-Oriented_Design/CallCenter.cpp
@@ -1,4 +1,100 @@
 #include <iostream>
+#include <vector>
+
+enum JobTitle {
+    Respondent = 0, Manager = 1, Director = 2
+};
+
+class Call;
+
+class Employee {
+    public:
+        Employee() = default;
+        Employee(JobTitle t) : job_title(t) {
+            free = true;
+        }
+        JobTitle job_title;
+        Call current_call;
+        bool is_free() {
+            return free;
+        }
+        void start_call(Call call) {
+            free = false;
+            current_call = call;
+        }
+        void finish_call() {
+            free = true;
+        }
+    private:
+        bool free;
+};
+
+class Caller {
+    public:
+        int phone_number;
+};
+
+class Call {
+    public:
+        Call(Caller c) : caller(c), title(JobTitle::Respondent) {}
+        Caller caller;
+        void set_handler(Employee e) {
+            handler = e;
+        }
+        void reply(std::string m) {
+        }
+        JobTitle get_title() {
+            return title;
+        }
+        void set_title(JobTitle t) {
+            title = t;
+        }
+        void increment_title(JobTitle t) {
+            title = JobTitle(title + 1);
+        }
+        void disconnect() {
+            
+        }
+    private:
+        Employee handler;
+        JobTitle title;
+};
+
+class Respondent : Employee {
+    Respondent() {
+        job_title = JobTitle::Respondent;
+    }
+};
+
+class Manager : Employee {
+    Manager() {
+        job_title = JobTitle::Manager;
+    }
+};
+
+class Director : Employee {
+    Director() {
+        job_title = JobTitle::Director;
+    }
+};
+
+std::vector<Employee> get_employees(int resp, int man, int dir) {
+   
+}
+
+class CallHandler {
+    public:
+        const int levels = 3;
+        const int num_of_repondents = 10;
+        const int num_of_managers = 4;
+        const int num_of_directors = 2;
+        std::vector<Employee> employees = get_employees(num_of_repondents, num_of_managers, num_of_directors);
+        void dispatch_call() {
+
+        }       
+};
+
+
 
 /**
  * Imagine you have a call center with three levels of employees: respondent, 
