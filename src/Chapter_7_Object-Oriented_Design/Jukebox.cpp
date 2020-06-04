9090734cde66d3756503e23e702542b7ca73b21e
Daria2002 <daria.matkovic@fer.hr>
2020-06-04 06:41:36 +0200

    Add some implementation for Jukebox

--- src/Chapter_7_Object-Oriented_Design/Jukebox.cpp
+++ src/Chapter_7_Object-Oriented_Design/Jukebox.cpp
@@ -10,67 +10,69 @@
 class User {
     public:
         User() = default;
-        User(std::string name, int pass) : user_name(name), password(pass) {}
-        bool check_password(const int pass) {
+        User(std::string name, std::string pass) : user_name(name), password(pass) {}
+        bool check_password(const std::string& pass) {
             return pass == password;
         }
         std::string name() {
             return user_name;
         }
     private:
-        int password;
+        std::string password;
         std::string user_name;
 };
 
 class Playlist;
 class CD;
 class Song;
+class SetOfSongs;
 
 class Jukebox {
     public:
-        std::vector<User> users;
         bool logged = false;
         User current_user;
-        bool login() {
+        void login() {
             std::string name;
+            std::cout << "** Login **\n";
             std::cout << "Enter name:\n";
             std::cin >> name;
-            if(User user; get_user(name, user)) {
+            User user; 
+            bool user_exists = get_user(name, user);
+            if(user_exists) {
                 std::cout << "Enter a password\n";
-                int pass;
+                std::string pass;
                 std::cin >> pass;
                 if(user.check_password(pass)) {
+                    current_user = user;
                     std::cout << "Password correct. You are logged in.\n";
+                    logged = true;
                 } else {
                     std::cout << "Password is not correct.\n";
                 }
-            } else {
-                std::cout << "User with username " << name << "doesn't exist\n";
+                return;
             }
-            logged = true;
+            std::cout << "User with username " << name << " doesn't exist\n";
         }
-        bool logout() {
+        void logout() {
             logged = false;
         }
-        bool sign_up() {
+        void sign_up() {
             std::string name;
-            int pass;
+            std::string pass;
+            std::cout << "** Sign up ** \n";
             std::cout << "Enter name:\n";
             std::cin >> name;
             std::cout << "Enter password:\n";
             std::cin >> pass;
-            users.push_back(User(name, pass));
-        }
-        void play(std::shared_ptr<CD> cd) {
-
-        }
-        void play(std::shared_ptr<Playlist> playlist) {
-
+            User u(name, pass);
+            users.push_back(u);
         }
         void add_cd(std::shared_ptr<CD> cd) {
             cds.push_back(cd);
         }
-    private:
+        void add_playlist(std::shared_ptr<Playlist> playlist) {
+            playlists.push_back(playlist);
+        }
         bool get_user(std::string name, User& user) {
             for(User& u : users) {
                 if(u.name() == name) {
@@ -80,22 +82,30 @@ class Jukebox {
             }
             return false;
         }
+        void play(std::shared_ptr<SetOfSongs>&&);
+    private:
+        std::vector<User> users;
         std::shared_ptr<CD> current_cd;
         std::vector<std::shared_ptr<CD>> cds;
+        std::vector<std::shared_ptr<Playlist>> playlists;
 };
 
-class SongSet {
+class SetOfSongs {
     public:
         std::vector<std::shared_ptr<Song>> songs;
         std::string name;
-        SongSet(std::vector<std::shared_ptr<Song>> s, std::string n) : songs(s), name(n) {}
+        SetOfSongs(std::vector<std::shared_ptr<Song>> s, std::string n) : songs(s), name(n) {}
 };
+
+void Jukebox::play(std::shared_ptr<SetOfSongs>&& set) {
+    std::cout << "Set " << set -> name << " is playing\n";
+}
 
 class Artist;
 
-class CD : public SongSet {
+class CD : public SetOfSongs {
     public:
-        CD(std::vector<std::shared_ptr<Song>> s, std::string n) : SongSet(s, n) {}
+        CD(std::vector<std::shared_ptr<Song>> s, std::string n) : SetOfSongs(s, n) {}
         std::shared_ptr<Artist> artist;
 };
 
@@ -105,8 +115,6 @@ class Song {
         std::string name;
         std::shared_ptr<Artist> artist;
 };
-
-inline bool operator==(const Song& lhs, const Song& rhs){ return true; }
 
 class Artist {
     public:
@@ -123,9 +131,9 @@ class Display {
     public:
 };
 
-class Playlist : public SongSet {
+class Playlist : public SetOfSongs {
     public:
-        Playlist(std::vector<std::shared_ptr<Song>> s, std::string n) : SongSet(s, n) {}
+        Playlist(std::vector<std::shared_ptr<Song>> s, std::string n) : SetOfSongs(s, n) {}
         void add_song(std::shared_ptr<Song> song) {
             songs.push_back(song);
         }
@@ -156,4 +164,12 @@ int main() {
     std::string str = "mix";
     Playlist p(pl, str);
     p.shuffle();
+
+    Jukebox jukebox;
+    jukebox.sign_up();
+    jukebox.login();
+    jukebox.add_playlist(std::make_shared<Playlist>(p));
+    std::shared_ptr<Playlist> playlist_ptr = std::make_shared<Playlist>(p);
+    jukebox.play(playlist_ptr);
+    jukebox.logout();
 }
