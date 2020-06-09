7ef91f00ea9559bd9cc143a93c4ab97bfad20d8c
Daria2002 <daria.matkovic@fer.hr>
2020-06-09 22:33:13 +0200

    Add solution for Online book reader

--- src/Chapter_7_Object-Oriented_Design/OnlineBookReader.cpp
+++ src/Chapter_7_Object-Oriented_Design/OnlineBookReader.cpp
@@ -26,12 +26,13 @@ class OnlineBookReaderSystem {
         std::shared_ptr<User> get_active_user() const {
             return active_user;
         }
+        bool is_book_finished();
         void set_active_book(std::shared_ptr<Book>);
         void set_active_user(std::shared_ptr<User>);
     private:
         std::shared_ptr<Library> library;
         std::shared_ptr<UserManager> user_manager;
-        std::shared_ptr<Display> display;
+        std::shared_ptr<Display> display = std::make_shared<Display>();
         std::shared_ptr<Book> active_book;
         std::shared_ptr<User> active_user;
 };
@@ -63,24 +64,28 @@ class User {
 class Book {
     public:
         Book() = default;
-        Book(int id) : book_id(id) {}
+        Book(int id, int pages) : book_id(id), num_of_pages(pages) {}
         int get_id() {
             return book_id;
         }
         void set_id(int i) {
             book_id = i;
         }
+        int number_of_pages() {
+            return num_of_pages;
+        }
     private:
         int book_id;
+        int num_of_pages;
 };
 
 class Library {
     public:
-        bool add_book(int id) {
+        bool add_book(int id, int num_of_pages) {
             if(books.find(id) != books.end()) { // books constains given id
                 return false; 
             }
-            books[id] = Book(id);
+            books[id] = Book(id, num_of_pages);
             return true;
         }
         bool remove_book(Book b) {
@@ -110,25 +115,39 @@ class Display {
             refresh_page();
         }
         void turn_page_backward() {
+            std::cout << "Turn page backward: ";
+            if(page_number <= 0) {
+                page_number = -1;
+                std::cout << "Book is closed\n";
+                return;
+            }
             page_number--;
             refresh_page();
         }
         void turn_page_forward() {
+            std::cout << "Turn page forward: ";
+            if(active_book -> number_of_pages() <= page_number) {
+                std::cout << "Book is finished\n";
+                return;
+            }
             page_number++;
             refresh_page();
+        }
+        bool book_read() {
+            return page_number >= (active_book -> number_of_pages());
         }
     private:
         int page_number = 0;
         std::shared_ptr<Book> active_book;
         std::shared_ptr<User> active_user;
         void refresh_username() {
-            std::cout << "username = " << active_user -> get_id() << '\n';
+            std::cout << "active user id = " << active_user -> get_id() << '\n';
         }
         void refresh_title() {
-            std::cout << "title = " << active_book -> get_id() << '\n';
+            std::cout << "active book id = " << active_book -> get_id() << '\n';
         }
         void refresh_page() {
-            std::cout << "Refreshed page = " << page_number << '\n';
+            std::cout << "current page = " << page_number << '\n';
         }
 };
 
@@ -155,9 +174,26 @@ void OnlineBookReaderSystem::set_active_book(std::shared_ptr<Book> book) {
     active_book = book;
     display -> display_book(book);
 }
+
 void OnlineBookReaderSystem::set_active_user(std::shared_ptr<User> user) {
     active_user = user;
     display -> display_user(user);
+}
+
+bool OnlineBookReaderSystem::is_book_finished() {
+    return display -> book_read(); 
+}
+
+void simulate_online_reader_system() {
+    OnlineBookReaderSystem online_book_reader;
+    online_book_reader.set_active_user(std::make_shared<User>(123, 1));
+    online_book_reader.set_active_book(std::make_shared<Book>(456, 10));
+    online_book_reader.get_display() -> turn_page_backward();
+    while (!online_book_reader.is_book_finished())
+    {
+        online_book_reader.get_display() -> turn_page_forward();
+    }   
+    online_book_reader.get_display() -> turn_page_forward();
 }
 
 /**
@@ -172,5 +208,5 @@ int main() {
     std::cout << "==================\n"
                  "Online Book Reader\n"
                  "==================\n";
-
+    simulate_online_reader_system();
 }
