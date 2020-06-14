ceacbe3ebabeca34a34143301993717b8db58d4e
Daria2002 <daria.matkovic@fer.hr>
2020-06-14 12:02:12 +0200

    Add some code for db in comments

--- src/Chapter_7_Object-Oriented_Design/ChatServer.cpp
+++ src/Chapter_7_Object-Oriented_Design/ChatServer.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <unordered_map>
 #include <memory>
+// #include "jdbc/mysql_driver.h"
+// #include "jdbc/cppconn/statement.h"
 
 enum UserStatusType {
     Offline, Away, Idle, Available, Busy
@@ -50,3 +52,31 @@ int main() {
     test_private_chat();
     test_group_chat();
 }
+
+/**
+ * example for db
+int main()
+{
+    sql::mysql::MySQL_Driver *driver;
+    sql::Connection *con;
+    sql::Statement *stmt;
+
+    driver = sql::mysql::get_mysql_driver_instance();
+    con = driver->connect("tcp://127.0.0.1:3306", "daria", "5555");
+    if(con->isValid()) {
+        std::cout << "Connection is valid\n";
+    } else {
+        std::cout << "Connection is invalid\n";
+    }
+
+    stmt = con -> createStatement();
+    stmt -> execute("USE my_test_db");
+    stmt->execute("DROP TABLE IF EXISTS test");
+    stmt->execute("CREATE TABLE test(id INT)");   
+    stmt -> execute("INSERT INTO my_test_table(name, age) VALUES (\"daria\", 25)");
+    
+    delete stmt;
+    delete con;
+    return 0;
+}
+*/
