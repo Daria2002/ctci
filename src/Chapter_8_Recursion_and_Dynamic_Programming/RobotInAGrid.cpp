b0de540f395fc353b365fd8c31086e52c9ca14e4
Daria2002 <daria.matkovic@fer.hr>
2020-06-24 06:42:05 +0200

    Start working on RobotInAGrid

---
+++ src/Chapter_8_Recursion_and_Dynamic_Programming/RobotInAGrid.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+void find_a_path() {
+
+}
+
+/**
+ * Imagine a robot sitting on the upper left corner of grid with r rows and c columns.
+ * The robot can only move in two directions, right and down, but certain cells are 
+ * "off limits" such that the robot cannot step on them. Design an algorithm to find a 
+ * path for the robot from top left to bottom right.
+ */ 
+int main() {
+    std::cout << "===============\nRobot in a grid\n===============\n";
+    find_a_path();
+}
