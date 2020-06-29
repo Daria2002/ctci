8770e05aab08d370edf6f204200b409f07b435b4
Daria Matković <dariamatkovicdaria@gmail.com>
2020-06-29 10:42:44 +0200

    Add solution for Towers Of Hanoi

--- src/Chapter_8_Recursion_and_Dynamic_Programming/TowersOfHanoi.cpp
+++ src/Chapter_8_Recursion_and_Dynamic_Programming/TowersOfHanoi.cpp
@@ -1,11 +1,36 @@
 #include <iostream>
+#include <stack> 
 
 class Tower {
     public:
+        Tower(std::string n) : name(n) {}
+        std::string name;
+        std::stack<int> disks;
+        bool add(int el) {
+            if(!disks.empty() && disks.top() <= el) {
+                return false;
+            }
+            disks.push(el);
+            return true;
+        }
+        int move_top() {
+            int el = disks.top();
+            disks.pop();
+            return el;
+        }
 };
 
-void move_disks(int n, Tower origin, Tower dest, Tower buffer) {
-    
+void move_top(Tower& origin, Tower& dest) {
+    int el = origin.move_top();
+    std::cout << "Move element " << el << " from " << origin.name << " to " << dest.name << ".\n";
+    dest.add(el);
+}
+
+void move_disks(int n, Tower& origin, Tower& dest, Tower& buffer) {
+    if(n <= 0) return;
+    move_disks(n - 1, origin, buffer, dest);
+    move_top(origin, dest);
+    move_disks(n - 1, buffer, dest, origin);
 }
 
 /**
@@ -19,5 +44,20 @@ void move_disks(int n, Tower origin, Tower dest, Tower buffer) {
  * Write a program to move the disks from the first tower to the last using Stacks.
  */
 int main() {
-
+    Tower origin("origin");
+    Tower dest("dest");
+    Tower buffer("buffer");
+    int size;
+    std::cout << "Enter size:\n";
+    std::cin >> size;
+    for(int i = size; i >= 1; i--) {
+        origin.add(i);
+    }
+    move_disks(size, origin, dest, buffer);
+    std::cout << "Destination elements:\n";
+    while (!dest.disks.empty()) {
+        std::cout << dest.disks.top() << ", ";
+        dest.disks.pop();
+    }
+    std::cout << '\n';
 }
