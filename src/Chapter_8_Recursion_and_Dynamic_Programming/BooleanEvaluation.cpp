dec1897825f6d3158c4d3e4c7b8d268f8d69b9b4
Daria2002 <daria.matkovic@fer.hr>
2020-07-05 13:48:24 +0200

    Add optimal solution (using memoization) for Boolean Evaluation

--- src/Chapter_8_Recursion_and_Dynamic_Programming/BooleanEvaluation.cpp
+++ src/Chapter_8_Recursion_and_Dynamic_Programming/BooleanEvaluation.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <unordered_map>
+#include <algorithm>
 
 bool string_to_bool(std::string str) {
     return str == "1" ? true : false;
@@ -39,9 +41,46 @@ int count_eval_bf(std::string expression, bool result) {
     return ways;
 }
 
+int count_eval_optimal(std::string expression, bool result, std::unordered_map<std::string, int>& hash_map) {
+    if(expression.size() == 0) return 0;
+    if(expression.size() == 1) return string_to_bool(expression) == result ? 1 : 0;
+
+    std::string key = (result == true ? "t" : "f") + expression;
+    std::unordered_map<std::string, int>::const_iterator got = hash_map.find(key);
+    if (got != hash_map.end()) return got -> second; // already int hash_map
+
+    int ways = 0;
+    for(int i = 1; i < expression.size(); i += 2) {
+        std::string left = expression.substr(0, i);
+        std::string right = expression.substr(i + 1);
+        int left_true = count_eval_optimal(left, true, hash_map);
+        int left_false = count_eval_optimal(left, false, hash_map);
+        int right_true = count_eval_optimal(right, true, hash_map);
+        int right_false = count_eval_optimal(right, false, hash_map);
+        int total = (left_true + left_false) * (right_true + right_false);
+        int total_true = 0;
+        char op = expression[i];
+        switch (op)
+        {
+        case '^': // XOR
+            total_true = (left_true * right_false) + (left_false * right_true);
+            break;
+        case '&': // AND 
+            total_true = left_true * right_true;
+            break;
+        default: // OR operation
+            total_true = left_true * right_true + left_true * right_false + left_false * right_true;
+            break;
+        }
+        ways += (result ? total_true : total - total_true); // if result == true take total_true, otherwise total_false
+    }
+    hash_map[key] = ways;
+    return ways;
+}
+
 int count_eval_optimal(std::string expression, bool result) {
-    // TODO
-    return 0;
+    std::unordered_map<std::string, int> hash_map;
+    return count_eval_optimal(expression, result, hash_map);
 }
 
 /**
