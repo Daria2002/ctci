e51aef1ca7af3767f43f30da848275a383df22a1
Daria Matković <dariamatkovicdaria@gmail.com>
2020-07-14 14:13:51 +0200

    Create MissingInt.cpp

---
+++ src/Chapter_10_Sorting_and_Searching/MissingInt.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+/**
+ * Given an input file with four billion non-negative integers, provide an algorithm to
+ * generate an integer that is not contained in the file. Assume you have 1 GB of memory available for
+ * this task.
+ * FOLLOW_UP:
+ * What if you have only 10MB of memory? Assume that all the values are distinct and we now have
+ * no more than one billion non-negative integers. 
+ */
+int main() {
+
+}
