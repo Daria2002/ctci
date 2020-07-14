ac9f642780a5b1cf99e332b3f83a220613e5c792
Daria Matković <dariamatkovicdaria@gmail.com>
2020-07-14 14:12:37 +0200

    Update SparseSearch.cpp

--- src/Chapter_10_Sorting_and_Searching/SparseSearch.cpp
+++ src/Chapter_10_Sorting_and_Searching/SparseSearch.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
 /**
- *  Given a sorted array of strings that is interspersed with empty strings, write a
+ * Given a sorted array of strings that is interspersed with empty strings, write a
  * method to find the location of a given string.
  * EXAMPLE:
  * Input: ball, {"at", "", "", "", "ball", "", "", "car", "", "", "dad", "", ""}
