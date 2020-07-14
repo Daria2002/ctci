dc1ef77f0532b426642534d6570a8cd5aa2dc414
Daria Matković <dariamatkovicdaria@gmail.com>
2020-07-14 14:06:01 +0200

    Update SortedMerge.cpp

--- src/Chapter_10_Sorting_and_Searching/SortedMerge.cpp
+++ src/Chapter_10_Sorting_and_Searching/SortedMerge.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
 /**
- *  You are given two sorted arrays, A and B, where A has a large enough buffer at the 
+ * You are given two sorted arrays, A and B, where A has a large enough buffer at the 
  * end to hold B. Write a method to merge B into A in sorted order. 
  */
 int main() {
