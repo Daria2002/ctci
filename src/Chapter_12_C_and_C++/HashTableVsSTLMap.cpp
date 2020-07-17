1da83c6597ee5f3d7f5e07adcf181a95a800fa8e
Daria Matković <dariamatkovicdaria@gmail.com>
2020-07-17 17:01:45 +0200

    Add solution for Hash Table Vs. STL Map

--- src/Chapter_12_C_and_C++/HashTableVsSTLMap.cpp
+++ src/Chapter_12_C_and_C++/HashTableVsSTLMap.cpp
@@ -6,5 +6,13 @@
  * options can be used instead of a hash table? 
  */
 int main() {
-
+    std::cout << "Hash table insert and look up complexity: O(1)\n"
+                 "STL map and binary search tree insert and look up complexity: O(log N)\n"
+                 "Hash table is implemented as an array of linkedin lists where each node\n"
+                 "holds two pieces of data: key and value. Array index is equal to\n"
+                 "hashFunction(key). If different keys has a same hash function there could be\n"
+                 "collision. That's the reason to store key and value in linked list node.\n"
+                 "If the number of inputs is small we can use binary search tree and STL map\n"
+                 "because even though they have O(log N) time complexity, the number of inputs\n"
+                 "may be small enough to make this time negligible";
 }
