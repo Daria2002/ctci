3d846752d63f6fbf4268ee68232d08cafda019ef
Daria Matković <dariamatkovicdaria@gmail.com>
2020-07-17 14:21:00 +0200

    Create HashTableVsSTLMap.cpp

---
+++ src/Chapter_12_C_and_C++/HashTableVsSTLMap.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+/**
+ * Compare and contrast a hash table and an STL map. How is a hash table 
+ * implemented? If the number of inputs is small, which data structure 
+ * options can be used instead of a hash table? 
+ */
+int main() {
+
+}
