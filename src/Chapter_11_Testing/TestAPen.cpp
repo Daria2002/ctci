b6e31fe30a06fbdca06a78e759b432befae41e63
Daria Matković <dariamatkovicdaria@gmail.com>
2020-07-17 11:19:45 +0200

    Add solution for Test a pen problem

---
+++ src/Chapter_11_Testing/TestAPen.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+/**
+ * How would you test a pen? 
+ */
+int main() {
+    std::cout << "Who is going to use the pen? What will they be doing with it (drawing or writing),\n"
+    "on what(paper, clothing, walls), what kind of tip does the pen have(felt, ballpoint),\n"
+    "is it intended to wash off the clothes or to be permanent, what colors are allowed?\n"
+    "Fact check: Verify that the pen is felt tip and that the ink is one of the allowed colors.\n"
+    "Intended use: Drawing. Does it write properly on clothes?\n"
+    "Intended use: Washing. Does it wash off? In hot, warm or cold water\n"
+    "Safety: Is the pen safe (non-toxic for children)\n"
+    "Unitended use: Test on other surfaces to check whether the behavior there is correct. They might\n"
+    "throw it or stomp on the pen.\n"
+    "It's important to test the intended and unintended scenarious, because people don't always use the\n"
+    "the product the way we want them to.\n";
+}
