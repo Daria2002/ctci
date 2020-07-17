8a68eafe8f8857064a1c1403e4cabe38de9d2cfd
Daria Matković <dariamatkovicdaria@gmail.com>
2020-07-17 14:30:36 +0200

    Create TwoDAlloc.cpp

---
+++ src/Chapter_12_C_and_C++/TwoDAlloc.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+/**
+ * Write a function in C called my2DAlloe which allocates a two-dimensional array. 
+ * Minimize the number of calls to malloc and make sure that the memory is accessible 
+ * by the notation arr[i][j]. 
+ */
+int main() {
+
+}
