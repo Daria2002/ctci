fbafad22603bf4d5b05a1e7b2ab430f8cf1a4b2c
Daria2002 <daria.matkovic@fer.hr>
2020-07-18 10:39:05 +0200

    Add solution for Shallow vs. Deep copy and Virtual functions

--- src/Chapter_12_C_and_C++/ShallowVsDeepCopy.cpp
+++ src/Chapter_12_C_and_C++/ShallowVsDeepCopy.cpp
@@ -1,9 +1,32 @@
 #include <iostream>
+#include <cstring>
+#include <cstdio>
+
+struct Test {
+    char* ptr;
+};
+
+void shallow_copy(Test& src, Test& dest) {
+    dest.ptr = src.ptr;
+}
+
+void deep_copy(Test& src, Test& dest) {
+    // allocate memory for new ponter
+    dest.ptr = (char*)malloc(strlen(src.ptr) + 1); // one more space for \0
+    strcpy(dest.ptr, src.ptr);
+}
 
 /**
  * What is the difference between deep copy and shallow copy? Explain how 
  * you would use each. 
  */
 int main() {
-
+    std::cout << "Shallow copy copies all the member values from one object\n"
+    "to another. A deep copy does all this and also deep copies any pointer\n"
+    "objects. The shallow copy can cause a lot of runtime errors, expecially\n"
+    "with the creation and deletion of objects. Shallow copy is used when\n"
+    "there is a need to pass information about a complex structure without\n"
+    "actual duplication of data. Shallow copy is rarely used. Deep copy\n"
+    "should be used in most cases, especially when the size of the copied\n"
+    "structure is small because there is enough memory space.\n";
 }
--- src/Chapter_12_C_and_C++/VirtualFunctions.cpp
+++ src/Chapter_12_C_and_C++/VirtualFunctions.cpp
@@ -1,8 +1,41 @@
 #include <iostream>
 
+class Shape {
+    public:
+        int edge_len;
+        virtual int circumference() {
+            std::cout << "Circumference of Base Class\n";
+            return 0;
+        }
+};
+
+class Triangle : public Shape {
+    public:
+        int circumference() {
+            std::cout << "Circumference of Triangle Class\n";
+            return 3 * edge_len;
+        }
+};
+
 /**
  * How do virtual functions work in C++? 
  */
 int main() {
-
+    Shape* x = new Shape();
+    x -> circumference(); // "Circumference of Base Class"
+    Shape* y = new Triangle();
+    y -> circumference(); // "Circumference of Triangle Class"
+    std::cout << "A virtual function depends on virtual function or vtable. If any function\n"
+    "of a class is declared to be virtual, a vtable is constructed which stores addressed of\n"
+    "the virtual functions of this class. The compiler also adds a hidden vptr variable in\n"
+    "all such classes which points to the vtable of that class. If a virtal function is not\n"
+    "overridden in the derived class, the vtable of the derived class stores address of the\n"
+    "in its parent class. The vtable is used to resolve the address of the function when the\n"
+    "virtual function is called. Dynamic binding in C++ is performed through the vtable mechanism.\n"
+    "Thus, when we assign the derived class object to the base class pointer, the vptr variable\n"
+    "points to the vtable of the derived class. This assignment ensures that the most derived\n"
+    "virtual function gets called. In the above example, circumference is a virtual function in\n"
+    "the Shape class, so it becomes virtual in each of the derived classes (Triangle, etc.).\n"
+    "C++ non-virtual function calls are resolved at compile time with static binding, while\n"
+    "virtual function calls are resolved at runtime with dynamic binding.\n";
 }
