59a3fed2783c8a2067730117ad3aeb98dc962984
Daria2002 <daria.matkovic@fer.hr>
2020-07-18 20:23:28 +0200

    Add solution for Copy Node problem

--- src/Chapter_12_C_and_C++/CopyNode.cpp
+++ src/Chapter_12_C_and_C++/CopyNode.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <unordered_map>
 
 class Node {
     public:
@@ -8,6 +9,30 @@ class Node {
         Node* ptr1;
         Node* ptr2;
 };
+
+// key - node address in the original structure,
+// value - node address in the new structure
+typedef std::unordered_map<Node*, Node*> NodeMap;
+
+Node* copy_recursive(Node* cur, NodeMap& map) {
+    if(cur == nullptr) return nullptr;
+    NodeMap::iterator it = map.find(cur);
+    // already copied
+    if(it != map.end()) {
+        return it -> second;
+    }
+    Node* node = new Node;
+    map[cur] = node;
+    node -> val = cur -> val;
+    node -> ptr1 = copy_recursive(cur -> ptr1, map);
+    node -> ptr2 = copy_recursive(cur -> ptr2, map);
+    return node;
+}
+
+Node* copy_structure(Node* root) {
+    NodeMap map;
+    return copy_recursive(root, map);
+}
 
 void initialize_node(Node** node) {
     *node = new Node(12);
@@ -32,4 +57,11 @@ void initialize_node(Node** node) {
 int main() {
     Node* root;
     initialize_node(&root);
+    std::cout << "Root val = " << root -> val << '\n';
+    std::cout << "Root ptr1 val = " << root -> ptr1 -> val << '\n';
+    std::cout << "Root ptr2 val = " << root -> ptr2 -> val << '\n';
+    Node* copy_root = copy_structure(root);
+    std::cout << "Copy root val = " << copy_root -> val << '\n';
+    std::cout << "Copy root ptr1 val = " << copy_root -> ptr1 -> val << '\n';
+    std::cout << "Copy root ptr2 val = " << copy_root -> ptr2 -> val << '\n';
 }
