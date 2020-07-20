01e5462a147f75035d7ae7513b87384772784613
Daria Matković <dariamatkovicdaria@gmail.com>
2020-07-20 14:04:07 +0200

    Add solution dor Design Grade Database

--- src/Chapter_14_Databases/DesignGradeDatabase.cpp
+++ src/Chapter_14_Databases/DesignGradeDatabase.cpp
@@ -7,5 +7,21 @@
  * their grade point average.
 */
 int main() {
-
+    std::cout << 
+    "This db will have 3 objects: Students, Course and Course Enrollment.\n"
+    "Course enrollment will pair Students and Courses. It has CourseID,\n"
+    "StudentID, Grade and Term fields. The code needs to return 10% of rows,\n"
+    "even if this means that our honor roll includes more than 10% of the class.\n"
+    "DECLARE @GPACutOff float;\n"
+    "SET @GPACutOff = (SELECT min(GPA) as 'GPAMin' FROM(\n"
+    "     SELECT TOP 10 PERCENT AVG(CourseEnrollment.Grade) AS GPA\n"
+    "     FROM CourseEnrollment\n"
+    "     GROUP BY CourseEnrollment.StudentID\n"
+    "     ORDER BY GPA desc) Grades);\n"
+    "SELECT StudentName, GPA\n"
+    "FROM (SELECT AVG(CourseEnrollment.Grade) AS GPA, CourseEnrollment.StudentID\n"
+    "      FROM CourseEnrollment\n"
+    "      GROUP BY CourseEnrollment.StudentID\n"
+    "      HAVING AVG(CourseEnrollment.Grade) >= @GPACutOff) Honors\n"
+    "INNER JOIN Students ON Honors.StudentID = Student.StudentID";
 }
