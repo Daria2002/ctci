97a230e8806c02201291f6121a9b88486f9b32f6
Daria Matković <dariamatkovicdaria@gmail.com>
2020-07-20 11:38:14 +0200

    Add solution for Entity-Relationship Diagram

--- src/Chapter_14_Databases/EntityRelationshipDiagram.cpp
+++ src/Chapter_14_Databases/EntityRelationshipDiagram.cpp
@@ -5,5 +5,11 @@
  * people, and professionals (people who work for companies).
 */
 int main() {
-
+    std::cout << 
+    "People will have attributes PID, Pname, Sex, DOB, Phone and Address.\n"
+    "Professional will have all attributes that People have and Degree and Experience.\n"
+    "Works for relationship can store attributes such as date of joining and salary.\n"
+    "Companies can have 3 attributes: CID, CName, Address. ISA = is a\n"
+    "People <-(ISA) Professional -(N) /Works for\\ -(1) Companies.\n"
+    "There is many to one relationship between pro and co.\n";
 }
