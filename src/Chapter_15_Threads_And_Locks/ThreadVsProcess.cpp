a7f23b024a735ffeb255ff00bd3d35924d7a0648
Daria2002 <daria.matkovic@fer.hr>
2020-07-21 20:04:59 +0200

    Add solution for Thread Vs Process

--- src/Chapter_15_Threads_And_Locks/ThreadVsProcess.cpp
+++ src/Chapter_15_Threads_And_Locks/ThreadVsProcess.cpp
@@ -4,4 +4,12 @@
  * Thread vs. Process: What's the difference between a thread and a process? 
 */
 int main() {
+    std::cout << 
+    "A process is an instance of a program in execution. Each\n"
+    "process is executed in a separated address space, and one\n"
+    "process cannot access the variables and data structures of\n"
+    "another process. To access the other process resources inter-process\n"
+    "communication have to be used. These include pipes, files, sockets etc.\n"
+    "A thread exists within a process and shares the process resource.\n"
+    "Multiple threads within the same process will share the same heap space.\n";
 }
