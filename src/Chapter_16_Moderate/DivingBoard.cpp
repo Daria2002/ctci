8fd3239f8764b26400407212cfb219fe8307bdb7
Daria2002 <daria.matkovic@fer.hr>
2020-11-19 11:55:38 +0100

    Add solution for DivingBoard

---
+++ src/Chapter_16_Moderate/DivingBoard.cpp
@@ -0,0 +1,61 @@
+#include<iostream>
+#include<unordered_set>
+
+void diving_board_recursive(int k, int total, const int shorter, const int longer, std::unordered_set<int>& lengths) {
+    if(k == 0) {
+        lengths.emplace(total);
+        return;
+    }
+    diving_board_recursive(k - 1, total + shorter, shorter, longer, lengths);
+    diving_board_recursive(k - 1, total + longer, shorter, longer, lengths);
+}
+
+std::unordered_set<int> diving_board_recursive(int k, const int shorter, const int longer) {
+    std::unordered_set<int> lengths;
+    diving_board_recursive(k, 0, shorter, longer, lengths);
+    return lengths;
+}
+
+std::unordered_set<int> diving_board_memoization(int k, const int shorter, const int longer) {
+
+}
+
+std::unordered_set<int> diving_board_optimal(int k, const int shorter, const int longer) {
+
+}
+
+void print_lengths(const std::unordered_set<int>& lengths, const int k) {
+    std::cout << "All possible lengths for the diving board made of " << k << " planks:\n";
+    for(int length : lengths) std::cout << length << '\n';
+}
+
+/**
+ * Diving Board: You are building a diving board by placing a bunch of planks of wood end-to-end.
+ * There are two types of planks, one of length shorter and one of length longer. You must use exactly
+ * K planks of wood. Write a method to generate all possible lengths for the diving board.
+ */
+int main() {
+    constexpr int shorter = 4;
+    constexpr int longer = 7;
+    int method;
+    std::cout << "Enter 1 to solve using recursive solution, 2 to solve using momoization solution or\n"
+    "any other number to solve optimal solution.\n";
+    std::cin >> method;
+    std::cout << "Enter the number of planks that should be used?\n";
+    int k;
+    std::cin >> k;
+    std::unordered_set<int> lengths;
+    switch (method)
+    {
+    case 1:
+        lengths = diving_board_recursive(k, shorter, longer);
+        break;
+    case 2:
+        lengths = diving_board_memoization(k, shorter, longer);
+        break;
+    default:
+        lengths = diving_board_optimal(k, shorter, longer);
+        break;
+    }
+    print_lengths(lengths, k);
+}
