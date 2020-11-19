c14349e3ca17187f8fe459e701027fc41caa142f
Daria2002 <daria.matkovic@fer.hr>
2020-11-19 12:23:09 +0100

    Add solution using memoization

--- src/Chapter_16_Moderate/DivingBoard.cpp
+++ src/Chapter_16_Moderate/DivingBoard.cpp
@@ -16,8 +16,27 @@ std::unordered_set<int> diving_board_recursive(int k, const int shorter, const i
     return lengths;
 }
 
-std::unordered_set<int> diving_board_memoization(int k, const int shorter, const int longer) {
+void diving_board_memoization(int k, int total, const int shorter, const int longer, 
+std::unordered_set<int>& lengths, std::unordered_set<std::string>& visited) {
+    if(k == 0) {
+        lengths.emplace(total);
+        return;
+    }
+    // we need to check total sum and num of planks because 1 + 1 != 2 (diff num of planks is remained)
+    std::string key = total + " " + k; 
+    if(visited.count(key)) {
+        return;
+    }
+    diving_board_memoization(k - 1, total + shorter, shorter, longer, lengths, visited);
+    diving_board_memoization(k - 1, total + longer, shorter, longer, lengths, visited);
+    visited.emplace(key);
+}
 
+std::unordered_set<int> diving_board_memoization(int k, const int shorter, const int longer) {
+    std::unordered_set<int> lengths;
+    std::unordered_set<std::string> visited;
+    diving_board_memoization(k, 0, shorter, longer, lengths, visited);
+    return lengths;
 }
 
 std::unordered_set<int> diving_board_optimal(int k, const int shorter, const int longer) {
