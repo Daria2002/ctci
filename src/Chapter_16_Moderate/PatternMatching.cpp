b36f414cf1bc47c28489e09dfddd4a282594d28c
Daria2002 <daria.matkovic@fer.hr>
2020-11-30 09:40:03 +0100

    Add alternate optimal solution for Pattern Matching

--- src/Chapter_16_Moderate/PatternMatching.cpp
+++ src/Chapter_16_Moderate/PatternMatching.cpp
@@ -24,12 +24,15 @@ bool is_match_brute_force(std::string pattern, std::string value)
 {
     if(pattern.size() == 0) return value.size() == 0;
     int size = value.size();
-    for(int mainSize = 0; mainSize < size; mainSize++) {
+    for(int mainSize = 0; mainSize < size; mainSize++) 
+    {
         // build string that is represented with letter a in pattern
         std::string mainStr = value.substr(0, mainSize + 1);
         // build string that is represented with letter b in pattern 
-        for(int altStart = mainSize; altStart <= size; altStart++) {
-            for(int altEnd = altStart; altEnd <= size; altEnd++) {
+        for(int altStart = mainSize; altStart <= size; altStart++) 
+        {
+            for(int altEnd = altStart; altEnd <= size; altEnd++) 
+            {
                 std::string altStr = value.substr(altStart, altEnd - altStart + 1);
                 if(value == build_with_pattern(pattern, mainStr, altStr)) {
                     return true;
@@ -74,10 +77,58 @@ bool is_match_optimal(std::string pattern, std::string value)
     return false;
 }
 
+bool is_equal(std::string s1, int offset1, int offset2, int size)
+{
+    for(int i = 0; i < size; i++)
+    {
+        if(s1[offset1 + i] != s1[offset2 + i])
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+bool matches(std::string pattern, std::string value, int main_size, int alt_size, int alt_start_index)
+{
+    int offset2 = main_size;
+    for(int i = 1; i < pattern.size(); i++) 
+    {
+        int size = pattern[i] == pattern[0] ? main_size : alt_size;
+        int offset1 = pattern[i] == pattern[0] ? 0 : alt_start_index;
+        if(!is_equal(value, offset1, offset2, size))
+        {
+            return false;
+        }
+        offset2 += size;
+    }
+    return true;
+}
+
 bool is_match_optimal_alternate(std::string pattern, std::string value)
 {
-    // todo
-    return true;
+    if(pattern.size() == 0) return value.size() == 0;
+    char main_char = pattern[0];
+    char alt_char = main_char == 'a' ? 'b' : 'a';
+    int size = value.size();
+    int count_main = count_letter(pattern, main_char);
+    int count_alt = count_letter(pattern, alt_char);
+    int first_alt_pattern = pattern.find(alt_char);
+    int max_main_size = size / count_main;
+    for(int main_size = 0; main_size <= max_main_size; main_size++)
+    {
+        int rem_len = size - main_size * count_main;
+        if(count_alt == 0 || rem_len % count_alt == 0)
+        {
+            int alt_start_index = main_size * first_alt_pattern;
+            int alt_size = count_alt == 0 ? 0 : rem_len / count_alt;
+            if(matches(pattern, value, main_size, alt_size, alt_start_index)) 
+            {
+                return true;
+            }
+        }
+    }
+    return false;
 }
 
 /**
