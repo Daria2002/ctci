1f73f8f23ade94202bd550440f64be6e23806444
Daria2002 <daria.matkovic@fer.hr>
2020-12-30 19:38:55 +0100

    Add solution using iteratively method

--- src/Chapter_17_Hard/RandomSet.cpp
+++ src/Chapter_17_Hard/RandomSet.cpp
@@ -16,10 +16,22 @@ std::vector<int> random_set_recursively(std::vector<int> original_set, int m, in
     return original_set;
 }
 
-std::vector<int> random_set_iteratively(std::vector<int> original_set, int m, int n)
+std::vector<int> random_set_iteratively(const std::vector<int>& original_set, const int m)
 {
-    // todo
-    return original_set;
+    std::vector<int> subset(m);
+    if(original_set.size() < m) return subset; // there are not enough elements, return empty vector
+    // fill in subset vecotr with first part of original vector
+    for(int i = 0; i < m; i++) subset[i] = original_set[i];
+    // go through rest of original vector
+    for(int i = m; i < original_set.size(); i++)
+    {
+        int k = rand() % (i + 1); // generate random number between [0, i]
+        if(k < m)
+        {
+            subset[k] = original_set[i];
+        }
+    }
+    return subset;
 }
 
 /**
@@ -35,7 +47,7 @@ int main()
     constexpr int n = 5;
     std::vector<int> original_set = get_original_set(n);
     constexpr int m = 3;
-    std::vector<int> random_set = (method == 1 ? random_set_recursively(original_set, m, n) : random_set_iteratively(original_set, m, n));
+    std::vector<int> random_set = (method == 1 ? random_set_recursively(original_set, m, n) : random_set_iteratively(original_set, m));
     std::cout << "Random set: ";
     for(int number : random_set) std::cout << number << ", ";
     std::cout << '\n';
