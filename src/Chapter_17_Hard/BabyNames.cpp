3bd77afbb4ff05550371f515e0c6a70e1e46f4b6
Daria2002 <daria.matkovic@fer.hr>
2021-01-05 10:15:51 +0100

    Add some code, but still not working

--- src/Chapter_17_Hard/BabyNames.cpp
+++ src/Chapter_17_Hard/BabyNames.cpp
@@ -1,14 +1,135 @@
 #include <iostream>
 #include <unordered_map>
 #include <vector>
+#include <unordered_set>
+
+class NameSet
+{
+    public:
+        NameSet() = default;
+        NameSet(std::string root, int frequency) : root_name(root), freq(frequency) 
+        {
+            names.insert(root);
+        }
+
+        std::string get_root_name() const
+        {
+            return root_name;
+        }
+
+        int get_freq() const
+        {
+            return freq;
+        }  
+
+        int size() const
+        {
+            return names.size();
+        }
+
+        std::unordered_set<std::string> get_names() const
+        {
+            return names;
+        }
+
+        void copy_names_with_freq(std::unordered_set<std::string> other_names, int frequency)
+        {
+            freq += frequency;
+            names.insert(other_names.begin(), other_names.end());
+        }
+    private:
+        std::unordered_set<std::string> names;
+        int freq = 0;
+        std::string root_name; 
+};
+
+inline bool operator==(const NameSet& set1, const NameSet& set2) { 
+    return set1.get_root_name() == set2.get_root_name();
+}
+
+inline bool operator!=(const NameSet& set1, const NameSet& set2) { 
+    return !(set1 == set2);
+}
+
+std::unordered_map<std::string, int> convertToMap(
+    const std::unordered_map<std::string, NameSet>& groups)
+{
+    std::unordered_map<std::string, int> list;
+    for(const auto pair : groups)
+    {
+        NameSet group = pair.second;
+        list[group.get_root_name()] = group.get_freq(); 
+    }
+    return list;
+}
+
+std::unordered_map<std::string, NameSet> construct_groups(
+    const std::unordered_map<std::string, int>& name_freq)
+{
+    std::unordered_map<std::string, NameSet> groups;
+    for(const auto pair : name_freq)
+    {
+        std::string name = pair.first;
+        int freq = pair.second;
+        NameSet group(name, freq);
+        groups[name] = group;
+    }
+    return groups;
+}
+
+void mergeClasses(
+    std::unordered_map<std::string, NameSet>& groups, 
+    const std::vector<std::pair<std::string, std::string>>& synonyms)
+{
+    for(auto group : groups)
+    {
+        std::cout << "group name = " << group.first << '\n';
+        std::cout << "group size = " << group.second.size() << '\n';
+    }
+    for(std::pair<std::string, std::string> synonym : synonyms)
+    {
+        std::string name1 = synonym.first;
+        std::cout << "name1 = " << name1 << '\n';
+        std::string name2 = synonym.second;
+        std::cout << "name2 = " << name2 << '\n';
+        NameSet set1 = (groups.count(name1) ? groups.at(name1) : NameSet());
+        NameSet set2 = (groups.count(name2) ? groups.at(name2) : NameSet());
+        if(set1 != set2 && set1.size() > 0 && set2.size() > 0)
+        {
+            std::cout << "merge\n";
+            // merge smaller set into the bigger one
+            NameSet smaller = (set2.size() < set1.size() ? set2 : set1);
+            NameSet bigger = (set2.size() < set1.size() ? set1 : set2);
+            // merge lists
+            std::unordered_set<std::string> other_names = smaller.get_names();
+            int freq = smaller.get_freq();
+            bigger.copy_names_with_freq(other_names, freq);
+            // update mapping
+            for(std::string name : other_names)
+            {
+                groups[name] = bigger;
+            }
+        }
+        else 
+        {
+            std::cout << "cannot merge\n";
+        }
+    }
+    std::cout << "after\n";
+    for(auto group : groups)
+    {
+        std::cout << "group name = " << group.first << '\n';
+        std::cout << "group size = " << group.second.size() << '\n';
+    }
+}
 
 std::unordered_map<std::string, int> name_freq_bf(
     std::unordered_map<std::string, int> name_freq, 
     std::vector<std::pair<std::string, std::string>> synonyms)
 {
-    std::unordered_map<std::string, int> map;
-    // todo
-    return map;
+    std::unordered_map<std::string, NameSet> groups = construct_groups(name_freq);
+    mergeClasses(groups, synonyms);
+    return convertToMap(groups);
 }
 
 std::unordered_map<std::string, int> name_freq_improved(
@@ -67,4 +188,8 @@ int main()
     std::vector<std::pair<std::string, std::string>> synonyms = get_synonyms();
     std::unordered_map<std::string, int> true_name_freq = 
     (method == 1 ? name_freq_bf(name_freq, synonyms) : name_freq_improved(name_freq, synonyms));
+    for(auto pair : true_name_freq)
+    {
+        std::cout << pair.first << " = " << pair.second << '\n';
+    }
 }
