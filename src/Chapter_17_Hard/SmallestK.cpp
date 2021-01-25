953b4759bc8d630e4250b36fc3f8cb444779da9b
Daria2002 <daria.matkovic@fer.hr>
2021-01-25 12:50:57 +0100

    Start working on approach using selection rank algorithm

--- src/Chapter_17_Hard/SmallestK.cpp
+++ src/Chapter_17_Hard/SmallestK.cpp
@@ -2,10 +2,13 @@
 #include <vector>
 #include <algorithm>
 #include "heap/MaxHeap.hpp"
+#include <cstdlib> // rand, srand
+#include <ctime> // time
 
 // O(nlogn)
 std::vector<int> smallest_sort(std::vector<int> elements, const int k)
 {
+    if(elements.size() <  k) return elements;
     std::sort(elements.begin(), elements.end());
     return std::vector<int>(elements.begin(), elements.begin() + k);
 }
@@ -21,7 +24,7 @@ std::vector<int> heap_to_vector(MaxHeap heap)
     return v;
 }
 
-MaxHeap get_k_max_heap(std::vector<int> elements, const int k)
+MaxHeap get_k_max_heap(const std::vector<int>& elements, const int k)
 {
     MaxHeap heap;
     for(int element : elements)
@@ -36,16 +39,89 @@ MaxHeap get_k_max_heap(std::vector<int> elements, const int k)
     return heap;
 }
 
-std::vector<int> smallest_max_heap(std::vector<int> elements, const int k)
+std::vector<int> smallest_max_heap(const std::vector<int>& elements, const int k)
 {
+    if(elements.size() <  k) return elements;
     MaxHeap heap = get_k_max_heap(elements, k);
     return heap_to_vector(heap);
 }
 
+int random_index(const int left, const int right)
+{
+    return (rand() % (right - left + 1) + left);
+}
+
+int max(std::vector<int> elements, int left, int right)
+{
+    int max_el = std::numeric_limits<int>::min();
+    for(int i = left; i <= right; i++)
+    {
+        if(elements[i] > max_el) max_el = elements[i];
+    }
+    return max_el;
+}
+
+void swap(std::vector<int>& elements, int n, int m)
+{
+    int tmp = elements[n];
+    elements[n] = elements[m];
+    elements[m] = tmp;
+}
+
+int partition(std::vector<int>& elements, int left, int right, int pivot)
+{
+    while (left <= right)
+    {
+        if(elements[left] > pivot) // left > pivot -> swap with right
+        {
+            swap(elements, left, right);
+            right--;
+        }
+        else if(elements[right] <= pivot) // right < pivot -> swap with left
+        {
+            swap(elements, left, right);
+            left++;
+        }
+        else // left and right in correct order
+        {
+            right--;
+            left++;
+        }
+    }
+    return left - 1;
+}
+
+int rank(std::vector<int> elements, int num_rank, int left, int right)
+{
+    int pivot = elements[random_index(left, right)];
+    int left_end_index = partition(elements, left, right, pivot);
+    int left_size = left_end_index - left + 1;
+    if(num_rank == left_size - 1)
+    {
+        return max(elements, left, left_end_index);
+    }
+    else if(num_rank > left_size - 1)
+    {
+        return rank(elements, num_rank - left_size, left_end_index + 1, right);
+    }
+    return rank(elements, num_rank, left, left_end_index); // num_rank < left_size - 1
+}
+
+int rank(std::vector<int> elements, const int k)
+{
+    return rank(elements, k, 0, elements.size() - 1);
+}
+
 std::vector<int> smallest_selection_rank1(std::vector<int> elements, const int k)
 {
-    // todo
-    return elements;
+    if(elements.size() <  k) return elements;
+    int biggest = rank(elements, k);
+    std::vector<int> smallest;
+    for(int i = 0; ((i < elements.size()) && (smallest.size() < k)); i++)
+    {
+        if(elements[i] <= biggest) smallest.push_back(elements[i]);
+    }
+    return smallest;
 }
 
 std::vector<int> smallest_selection_rank2(std::vector<int> elements, const int k)
@@ -68,6 +144,7 @@ int main()
     std::cin >> k;
     std::vector<int> elements = {8, 6, 17, 5, 1, 6, 12, 2};
     std::vector<int> smallest;
+    srand(time(NULL));
     if(method == 1)
     {
         smallest = smallest_sort(elements, k);
