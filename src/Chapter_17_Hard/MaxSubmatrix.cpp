2128426e7f4aa55e1f830ab44ba103c14298ad66
Daria2002 <daria.matkovic@fer.hr>
2021-02-22 07:12:30 +0100

    Add bf solution

--- src/Chapter_17_Hard/MaxSubmatrix.cpp
+++ src/Chapter_17_Hard/MaxSubmatrix.cpp
@@ -5,13 +5,49 @@ class SubMatrix
 {
     public:
         SubMatrix() = default;
-        SubMatrix(int r1, int c1, int r2, int c2) : row1(r1), col1(c1), row2(r2), col2(c2) {}
-        int row1, col1, row2, col2;
+        SubMatrix(int r1, int c1, int r2, int c2, int s) : row1(r1), col1(c1), row2(r2), col2(c2), sum(s) 
+        {
+            initialized = true;
+        }
+        int row1, col1, row2, col2, sum;
+        bool initialized = false;
 };
 
-SubMatrix max_submatrix_bf(std::vector<std::vector<int>> matrix)
+int sum_submatrix(const std::vector<std::vector<int>>& matrix, const int row1, const int col1, const int row2, const int col2)
 {
-    // todo
+    int sum = 0; 
+    for(int r = row1; r <= row2; r++)
+    {
+        for(int c = col1; c <= col2; c++)
+        {
+            sum += matrix[r][c];
+        }
+    }
+    return sum;
+}
+
+SubMatrix max_submatrix_bf(const std::vector<std::vector<int>>& matrix)
+{
+    int count = matrix.size();
+    SubMatrix best;
+    for(int row1 = 0; row1 < count; row1++)
+    {
+        for(int row2 = 0; row2 < count; row2++)
+        {
+            for(int col1 = 0; col1 < count; col1++)
+            {
+                for(int col2 = 0; col2 < count; col2++)
+                {
+                    int sum = sum_submatrix(matrix, row1, col1, row2, col2);
+                    if(!best.initialized || best.sum < sum)
+                    {
+                        best = SubMatrix(row1, col1, row2, col2, sum);
+                    }   
+                }
+            }
+        }
+    }
+    return best;
 }
 
 SubMatrix max_submatrix_dp(std::vector<std::vector<int>> matrix)
@@ -35,8 +71,13 @@ int main()
     "other number to solve using optimized solution:\n";
     std::cin >> method;
     SubMatrix max_submatrix;
-    std::vector<std::vector<int>> matrix = {{1, 4, 5, 2, -3, -7}, {-1, 5, 1, 2, 0, 1}, {2, 4, 2, -8, -1, -1}, 
-    {-1, -2, 2, 3, 1, 4}, {1, 4, 2, 7, -10, 9}, {7, 4, -8, 8, -4, 1}};
+    std::vector<std::vector<int>> matrix = {
+        {1, 4, 5, 2, -3, -7}, 
+        {-1, 5, 1, 2, 0, 1}, 
+        {2, 4, 2, -8, -1, -1}, 
+        {-1, -2, 2, 3, 1, 4}, 
+        {1, 4, 2, 7, -10, 9}, 
+        {7, 4, -8, 8, -4, 1}};
     if(method == 1)
     {
         max_submatrix = max_submatrix_bf(matrix);
