143261d79747cd3eeee1a9397472bec470571806
Daria2002 <daria.matkovic@fer.hr>
2021-03-01 14:29:52 +0100

    Start working on wordrectangle

---
+++ src/Chapter_17_Hard/WordRectangle.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+/**
+ * Word Rectangle: Given a list of millions of words, design an algorithm to create the largest possible
+ * rectangle of letters such that every row forms a word (reading left to right) and every column forms 
+ * a word (reading top to bottom). The words need not be choosen consecutively from the list, but all rows
+ * must be the same length and all columns must be the same height.
+ */
+int main()
+{
+
+}
