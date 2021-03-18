288201ec64c1f5faba0a89cb900831f366b5c850
Daria2002 <daria.matkovic@fer.hr>
2021-03-18 09:50:50 +0100

    Add some minor things for SparseSimilarity

--- src/Chapter_17_Hard/SparseSimilarity.cpp
+++ src/Chapter_17_Hard/SparseSimilarity.cpp
@@ -1,4 +1,57 @@
 #include <iostream>
+#include <vector>
+
+class Document
+{
+    public:
+        int document_id;
+        std::vector<int> elements;
+        Document() = default;
+        Document(int id, std::vector<int> el) : document_id(id), elements(el) {}
+};
+
+std::vector<Document> get_documents()
+{
+    std::vector<Document> documents;
+    documents.push_back(Document(13, {14, 15, 100, 9, 3}));
+    documents.push_back(Document(32, {32, 1, 9, 3, 5}));
+    documents.push_back(Document(19, {15, 29, 2, 6, 8, 7}));
+    documents.push_back(Document(24, {7, 10}));
+    return documents;
+}
+
+class Similarity
+{
+    public:
+        int id1, id2, similarity;
+        Similarity() = default;
+        Similarity(int i1, int i2, int s) : id1(i1), id2(i2), similarity(s) {}
+};
+
+std::vector<Similarity> similarities_bf(std::vector<Document> documents)
+{
+    // todo
+}
+
+std::vector<Similarity> similarities_better_bf(std::vector<Document> documents)
+{
+    // todo
+}
+
+std::vector<Similarity> similarities_little_optimization(std::vector<Document> documents)
+{
+    // todo
+}
+
+std::vector<Similarity> similarities_optimized(std::vector<Document> documents)
+{
+    // todo
+}
+
+std::vector<Similarity> similarities_alternative_optimized(std::vector<Document> documents)
+{
+    // todo
+}
 
 /**
  * Sparse Similarity: The similarity of two documents (each with distinct words) is defined
@@ -33,4 +86,31 @@ int main()
     "optimization, 4 for optimized solution or any other number for alternative optimized solution:\n";
     int method;
     std::cin >> method;
+    std::vector<Document> documents = get_documents();
+    std::vector<Similarity> similarities;
+    if(method == 1)
+    {
+        similarities = similarities_bf(documents);
+    }
+    else if(method == 2)
+    {
+        similarities = similarities_better_bf(documents);
+    }
+    else if(method == 3)
+    {
+        similarities = similarities_little_optimization(documents);
+    }
+    else if(method == 4)
+    {
+        similarities = similarities_optimized(documents);
+    }
+    else 
+    {
+        similarities = similarities_alternative_optimized(documents);
+    }
+    std::cout << "ID1, ID2: SIMILARITY\n";
+    for(Similarity s : similarities)
+    {
+        std::cout << s.id1 << ", " << s.id2 << ": " << s.similarity << '\n';
+    }
 }
